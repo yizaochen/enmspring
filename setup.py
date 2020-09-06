91660fcf805045c1a863b47223bd20d7f5ecc045
Yizao <yizaochen@gmail.com>
2020-09-06 20:30:14 +0800

    Scatter Plot

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='enmspring', 
+      version='0.1',
+      packages=find_packages(),
+      url='https://github.com/yizaochen/enmspring.git',
+      author='Yizao Chen',
+      author_email='yizaochen@gmail.com',
+      install_requires=[
+          'MDAnalysis',
+          'matplotlib',
+          'pandas'
+      ]
+      )
