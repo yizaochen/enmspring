73696f91d971779641c31decb668d584b4b7661b
Yizao <yizaochen@gmail.com>
2020-09-07 17:52:28 +0800

    Box Plot

--- enmspring/k_b0_plot.py
+++ enmspring/k_b0_plot.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from spring import Spring
 import k_b0_util
 
-
 class ScatterPlot:
     hosts = ['a_tract_21mer', 'ctct_21mer', 'gcgc_21mer',
              'g_tract_21mer', 'atat_21mer', 'tgtg_21mer']
@@ -111,4 +110,89 @@ class ScatterPlot:
             ax.set_xlabel(xlabel, fontsize=lbfz)
         if host in ['a_tract_21mer', 'g_tract_21mer']:
             ax.set_ylabel(ylabel, fontsize=lbfz)
+
+class BoxPlot:
+    hosts = ['a_tract_21mer', 'ctct_21mer', 'gcgc_21mer',
+             'g_tract_21mer', 'atat_21mer', 'tgtg_21mer']
+    n_bp = 21
+    type_na = 'bdna+bdna'
+    
+    def __init__(self, rootfolder, cutoff):
+        self.rootfolder = rootfolder
+        self.cutoff = cutoff
+        self.d_df = self.__initialize_df()
+
+    def plot_main(self, category, axes, key, nrows, ncols):
+        """
+        category: PP, bp, st, RB, R, PB
+        key: b0, k
+        """
+        subcategories = k_b0_util.category_dict[category]
+        d_axes = self.__get_axes_index(axes, category, nrows, ncols)
+        for subcategory in subcategories:
+            ax = d_axes[subcategory]
+            self.__boxplot_subcategory(ax, subcategory, category, key)
+            self.__set_xticklabels(ax)
+            self.__set_ylabel(ax, key)
+            ax.set_title(f'{subcategory}', fontsize=14)
+        return d_axes
+
+    def __boxplot_subcategory(self, ax, subcategory, category, key):
+        """
+        key: b0, k
+        """
+        x = list()
+        for host in self.hosts:
+            df_temp = self.d_df[host]
+            k_b0_dict = k_b0_util.get_k_b0_mean_std_by_category(df_temp, category)
+            if len(k_b0_dict[subcategory]['k']) > 2:
+                x.append(k_b0_dict[subcategory][key])
+            else:
+                x.append(list())
+        ax.boxplot(x)
+
+    def __set_xticklabels(self, ax):
+        xticklabels = self.hosts
+        xticks = range(1, len(xticklabels)+1)
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(xticklabels)
+
+    def __set_ylabel(self, ax, key):
+        lbfz = 12
+        d_label = {'b0': r'$b_{m}^{0}$ ($\mathrm{\AA}$) in dsDNA',
+                   'k': r'$k_m$ (kcal/mol/$\mathrm{\AA^{2}}$)'}
+        ylabel = d_label[key]
+        ax.set_ylabel(ylabel, fontsize=lbfz)
+
+    def __initialize_df(self):
+        d_temp = dict()
+        for host in self.hosts:
+            spring_obj = Spring(self.rootfolder, host, self.type_na, self.n_bp)
+            df0 = spring_obj.read_k_b0_pairtype_df_given_cutoff(self.cutoff)
+            d_temp[host] = k_b0_util.get_central_bps_df(df0)
+        return d_temp
+
+    def __get_axes_index(self, axes, category, nrows, ncols):
+        subcategories = k_b0_util.category_dict[category]
+        d_axes = dict()
+        idx = 0
+        for row_id in range(nrows):
+            for col_id in range(ncols):
+                subcategory = subcategories[idx]
+                if (nrows == 1) & (ncols != 1):
+                    d_axes[subcategory] = axes[col_id]
+                elif (nrows == 1) & (ncols == 1):
+                    d_axes[subcategory] = axes
+                else:
+                    d_axes[subcategory] = axes[row_id, col_id]
+                idx += 1
+        return d_axes
+
+    def __initialize_df(self):
+        d_temp = dict()
+        for host in self.hosts:
+            spring_obj = Spring(self.rootfolder, host, self.type_na, self.n_bp)
+            df0 = spring_obj.read_k_b0_pairtype_df_given_cutoff(self.cutoff)
+            d_temp[host] = k_b0_util.get_central_bps_df(df0)
+        return d_temp
 
