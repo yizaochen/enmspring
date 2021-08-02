832f8892d3767514ee826a8db5191209eccdb32b
yizaochen <yizaochen@gmail.com>
2021-08-02 11:28:15 +0800

    Update

---
+++ enmspring/abbr.py
@@ -0,0 +1,7 @@
+class Abbreviation:
+    d_map = {'a_tract_21mer': 'A-tract', 'tat_21mer': 'A-junction', 
+             'g_tract_21mer': 'G-tract', 'gcgc_21mer': 'CpG'}
+    
+    @staticmethod
+    def get_abbreviation(host):
+        return Abbreviation.d_map[host]
--- enmspring/graphs_bigtraj.py
+++ enmspring/graphs_bigtraj.py
@@ -2,7 +2,7 @@ from os import path
 import MDAnalysis
 import numpy as np
 from enmspring.spring import Spring
-from enmspring.graphs import Stack
+from enmspring.graphs import Stack, BackboneRibose
 from enmspring.na_seq import sequences
 from enmspring.miscell import check_dir_exist_and_make
 
@@ -17,22 +17,35 @@ class StackMeanModeAgent:
         
         self.host_folder = path.join(rootfolder, host)
         self.npy_folder = path.join(self.host_folder, 'mean_mode_npy')
-        self.f_laplacian = path.join(self.npy_folder, 'laplacian.npy')
+        self.f_laplacian = self.set_f_laplacian()
         self.check_folders()
 
         self.time_list = self.get_time_list()
         self.n_window = len(self.time_list)
         self.d_smallagents = self.get_all_small_agents()
 
+        self.node_list = None
+        self.d_idx = None
         self.n_node = None
+
         self.laplacian_mat = None
 
         self.w = None  # Eigenvalue array
         self.v = None  # Eigenvector matrix, the i-th column is the i-th eigenvector
 
+        self.strand1_array = list() # 0: STRAND1, 1: STRAND2
+        self.strand2_array = list() #
+        self.strand1_benchmark = None
+        self.strand2_benchmark = None
+
+        self.d_seq = {'STRAND1': sequences[host]['guide'], 'STRAND2': sequences[host]['target']}
+
     def check_folders(self):
         for folder in [self.npy_folder]:
             check_dir_exist_and_make(folder)
+
+    def set_f_laplacian(self):
+        return path.join(self.npy_folder, 'laplacian.npy')
 
     def get_time_list(self):
         middle_interval = int(self.interval_time/2)
@@ -53,6 +66,13 @@ class StackMeanModeAgent:
     def preprocess_all_small_agents(self):
         for time1, time2 in self.time_list:
             self.d_smallagents[(time1,time2)].pre_process()
+        self.n_node = self.d_smallagents[(time1,time2)].n_node
+
+    def process_first_small_agent(self):
+        time1, time2 = self.time_list[0]
+        self.d_smallagents[(time1,time2)].pre_process()
+        self.node_list = self.d_smallagents[(time1,time2)].node_list
+        self.d_idx = self.d_smallagents[(time1,time2)].d_idx
         self.n_node = self.d_smallagents[(time1,time2)].n_node
 
     def make_mean_mode_laplacian(self):
@@ -82,7 +102,59 @@ class StackMeanModeAgent:
     def get_eigenvector_by_id(self, sele_id):
         return self.v[:,sele_id-1]
 
+    def set_benchmark_array(self):
+        idx_start_strand2 = self.d_idx['B6']
+        strand1 = np.zeros(self.n_node)
+        strand2 = np.zeros(self.n_node)
+        strand1[:idx_start_strand2] = 1.
+        strand2[idx_start_strand2:] = 1.
+        self.strand1_benchmark = strand1
+        self.strand2_benchmark = strand2
 
+    def decide_eigenvector_strand(self, eigv_id):
+        eigv = self.get_eigenvector_by_id(eigv_id)
+        dot_product = np.dot(eigv, self.strand1_benchmark)
+        if np.isclose(dot_product, 0.):
+            return True #'STRAND2'
+        else:
+            return False #'STRAND1'
+
+    def set_strand_array(self):
+        for eigv_id in range(1, self.n_node+1):
+            if self.decide_eigenvector_strand(eigv_id):
+                self.strand2_array.append(eigv_id)
+            else:
+                self.strand1_array.append(eigv_id)
+        print(f'Total number of nodes: {self.n_node}')
+        print(f'There are {len(self.strand1_array)} eigenvectors belonging to STRAND1.')
+        print(f'There are {len(self.strand2_array)} eigenvectors belonging to STRAND2.')
+        print(f'Sum of two strands: {len(self.strand1_array)+len(self.strand2_array)}')
+
+    def get_lambda_by_strand(self, strandid):
+        if strandid == 'STRAND1':
+            return [self.get_eigenvalue_by_id(eigv_id) for eigv_id in self.strand1_array]
+        else:
+            return [self.get_eigenvalue_by_id(eigv_id) for eigv_id in self.strand2_array]
+
+class BackboneMeanModeAgent(StackMeanModeAgent):
+    def set_f_laplacian(self):
+        return path.join(self.npy_folder, 'laplacian_backbone.npy')
+    
+    def get_all_small_agents(self):
+        d_smallagents = dict()
+        for time1, time2 in self.time_list:
+            time_label = f'{time1}_{time2}'
+            d_smallagents[(time1,time2)] = BackboneGraph(self.host, self.rootfolder, time_label)
+        return d_smallagents
+
+    def set_benchmark_array(self):
+        idx_start_strand2 = self.d_idx['B1']
+        strand1 = np.zeros(self.n_node)
+        strand2 = np.zeros(self.n_node)
+        strand1[:idx_start_strand2] = 1.
+        strand2[idx_start_strand2:] = 1.
+        self.strand1_benchmark = strand1
+        self.strand2_benchmark = strand2
 
 class StackGraph(Stack):
     def __init__(self, host, rootfolder, time_label):
@@ -97,6 +169,42 @@ class StackGraph(Stack):
         self.spring_obj = Spring(self.rootfolder, self.host, self.type_na, self.n_bp, time_label)
         self.df_all_k = self.spring_obj.read_k_b0_pairtype_df_given_cutoff(self.cutoff)
         self.df_st = self.read_df_st()
+
+        self.crd = path.join(self.input_folder, '{0}.nohydrogen.avg.crd'.format(self.type_na))
+        self.npt4_crd = path.join(self.input_folder, '{0}.nohydrogen.crd'.format(self.type_na))
+        self.u = MDAnalysis.Universe(self.crd, self.crd)
+        self.map, self.inverse_map, self.residues_map, self.atomid_map,\
+        self.atomid_map_inverse, self.atomname_map, self.strandid_map,\
+        self.resid_map, self.mass_map = self.build_map()
+
+        self.node_list = None
+        self.d_idx = None
+        self.n_node = None
+        self.adjacency_mat = None
+        self.degree_mat = None
+        self.laplacian_mat = None
+
+        self.w = None  # Eigenvalue array
+        self.v = None  # Eigenvector matrix, the i-th column is the i-th eigenvector
+        self.strand1_array = list() # 0: STRAND1, 1: STRAND2
+        self.strand2_array = list() #
+        self.strand1_benchmark = None
+        self.strand2_benchmark = None
+
+        self.d_seq = {'STRAND1': sequences[host]['guide'], 'STRAND2': sequences[host]['target']}
+
+class BackboneGraph(BackboneRibose):
+    def __init__(self, host, rootfolder, time_label):
+        self.host = host
+        self.rootfolder = rootfolder
+        self.time_label = time_label
+
+        self.host_folder = path.join(rootfolder, host)
+        self.na_folder = path.join(self.host_folder, self.type_na, time_label)
+        self.input_folder = path.join(self.na_folder, 'input')
+
+        self.spring_obj = Spring(self.rootfolder, self.host, self.type_na, self.n_bp, time_label)
+        self.df_all_k = self.spring_obj.read_k_b0_pairtype_df_given_cutoff(self.cutoff)
 
         self.crd = path.join(self.input_folder, '{0}.nohydrogen.avg.crd'.format(self.type_na))
         self.npt4_crd = path.join(self.input_folder, '{0}.nohydrogen.crd'.format(self.type_na))
