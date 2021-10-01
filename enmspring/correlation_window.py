3e0ec3df0ea9a102b4cac107d83bddc93f83c35a
yizaochen <yizaochen@gmail.com>
2021-10-01 20:05:15 +0800

    Update

--- enmspring/correlation_window.py
+++ enmspring/correlation_window.py
@@ -1,6 +1,7 @@
 from os import path
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from enmspring.hb_util import HBAgentBigTraj
 from enmspring.graphs_bigtraj import BackboneMeanModeAgent, StackMeanModeAgent
 from enmspring.na_seq import sequences
@@ -14,6 +15,7 @@ class HBAgent:
 
     resid_lst = list(range(4, 19))
     type_lst = ['type1', 'type2', 'type3']
+    d_abbr_hb = {'HB1': 'type1', 'HB2': 'type2', 'HB3': 'type3'}
 
     def __init__(self, host, bigtraj_folder, corr_folder):
         self.host = host
@@ -25,6 +27,18 @@ class HBAgent:
         self.df = None
 
         self.hb_reader = None
+
+    def get_hb_array(self, interaction, strand_id):
+        typename = self.d_abbr_hb[interaction]
+        data_lst = list()
+        if strand_id == 'STRAND1':
+            resid_lst = list(range(4, 19))
+        else:
+            resid_lst = list(range(18, 3, -1))
+        for resid in resid_lst:
+            key = f'{resid}-{typename}'
+            data_lst += self.df[key].tolist()
+        return np.array(data_lst)
 
     def set_hb_reader(self):
         self.hb_reader = HBAgentBigTraj(self.host, self.bigtraj_folder, self.n_bp, self.only_central, self.split_5, self.one_big_window, self.interval_time)
@@ -108,6 +122,27 @@ class BackboneAgent:
         self.first_small_agent = None
 
         self.d_seq = {'STRAND1': sequences[host]['guide'], 'STRAND2': sequences[host]['target']}
+
+    def get_bb1tobb3_array(self, interaction, upper_or_lower):
+        if upper_or_lower == 'upper':
+            resid_lst = range(3, 18)
+        else:
+            resid_lst = range(4, 19)
+        data_lst = list()
+        for resid_i in resid_lst:
+            resid_j = resid_i + 1
+            key = f'{resid_i}-{resid_j}-{interaction}'
+            data_lst += self.df[key].tolist()
+        return np.array(data_lst)
+
+    def get_bb4tobb5_array(self, interaction):
+        resid_lst = list(range(4, 19))
+        data_lst = list()
+        for resid_i in resid_lst:
+            resid_j = resid_i
+            key = f'{resid_i}-{resid_j}-{interaction}'
+            data_lst += self.df[key].tolist()
+        return np.array(data_lst)
 
     def make_df(self):
         self.set_laplacian_from_small_agents()
@@ -194,6 +229,18 @@ class StackAgent(BackboneAgent):
 
     mean_mode_obj = StackMeanModeAgent
 
+    def get_st1_array(self, interaction, upper_or_lower):
+        if upper_or_lower == 'upper':
+            resid_lst = range(3, 18)
+        else:
+            resid_lst = range(4, 19)
+        data_lst = list()
+        for resid_i in resid_lst:
+            resid_j = resid_i + 1
+            key = f'{resid_i}-{resid_j}-{interaction}'
+            data_lst += self.df[key].tolist()
+        return np.array(data_lst)
+
     def get_f_df(self):
         return path.join(self.corr_input_folder, f'{self.host}_{self.strand_id}_basestack.csv')
 
@@ -209,7 +256,10 @@ class StackAgent(BackboneAgent):
 
 
 class CorrelationScatterv0:
+    d_abbr_host = {'a_tract_21mer': 'A-tract', 'g_tract_21mer': 'G-tract', 'atat_21mer': 'TATA', 'gcgc_21mer': 'CpG'}
     strand_lst = ['STRAND1', 'STRAND2']
+    lbfz = 12
+    ttfz = 14
 
     def __init__(self, host, bigtraj_folder, corr_folder):
         self.host = host
@@ -219,21 +269,86 @@ class CorrelationScatterv0:
         self.hb_agent = None
         self.ini_hb_agent()
 
-        self.d_stack_agent = self.ini_d_stack_agent()
-        self.d_backbone_agent = self.ini_d_backbone_agent()
+        self.d_stack_agent = None
+        self.ini_d_stack_agent()
+        self.d_backbone_agent = None
+        self.ini_d_backbone_agent()
+
+    def scatter_single(self, strand_id, interaction_x, interaction_y, figsize, upper_or_lower_bb='upper', upper_or_lower_st='upper'):
+        """
+        interaction_x, interaction_y: HB1, HB2, HB3, BB1-BB5, ST1
+        """
+        fig, ax = plt.subplots(figsize=figsize)
+        x_array = self.get_data_array_by_interaction(interaction_x, strand_id, upper_or_lower_bb, upper_or_lower_st)
+        y_array = self.get_data_array_by_interaction(interaction_y, strand_id, upper_or_lower_bb, upper_or_lower_st)
+        ax.scatter(x_array, y_array)
+        ax.set_xlabel(f'{interaction_x} (kcal/mol/Å$^2$)', fontsize=self.lbfz)
+        ax.set_ylabel(f'{interaction_y} (kcal/mol/Å$^2$)', fontsize=self.lbfz)
+        host_abbr = self.d_abbr_host[self.host]
+        ax.set_title(f'{host_abbr} {strand_id}', fontsize=self.ttfz)
+        return fig, ax
+
+    def get_data_array_by_interaction(self, interaction, strand_id, upper_or_lower_bb, upper_or_lower_st):
+        if interaction in ['HB1', 'HB2', 'HB3']:
+            data_array = self.hb_agent.get_hb_array(interaction, strand_id)
+        elif interaction in ['BB1', 'BB2', 'BB3']:
+            data_array = self.d_backbone_agent[strand_id].get_bb1tobb3_array(interaction, upper_or_lower_bb)
+        elif interaction in ['BB4', 'BB5']:
+            data_array = self.d_backbone_agent[strand_id].get_bb4tobb5_array(interaction)
+        else:
+            data_array = self.d_stack_agent[strand_id].get_st1_array(interaction, upper_or_lower_st)
+        return data_array
 
     def ini_hb_agent(self):
         self.hb_agent = HBAgent(self.host, self.bigtraj_folder, self.corr_folder)
         self.hb_agent.read_df()
 
     def ini_d_stack_agent(self):
-        d_stack_agent = dict()
+        self.d_stack_agent = dict()
         for strand_id in self.strand_lst:
-            d_stack_agent[strand_id] = None
-        return d_stack_agent
+            self.d_stack_agent[strand_id] = StackAgent(self.host, strand_id, self.bigtraj_folder, self.corr_folder)
+            self.d_stack_agent[strand_id].read_df()
 
     def ini_d_backbone_agent(self):
-        d_backbone_agent = dict()
+        self.d_backbone_agent = dict()
         for strand_id in self.strand_lst:
-            d_backbone_agent[strand_id] = None
-        return d_backbone_agent
+            self.d_backbone_agent[strand_id] = BackboneAgent(self.host, strand_id, self.bigtraj_folder, self.corr_folder)
+            self.d_backbone_agent[strand_id].read_df()
+
+class BigScatterv0:
+    d_abbr_host = {'a_tract_21mer': 'A-tract', 'g_tract_21mer': 'G-tract', 'atat_21mer': 'TATA', 'gcgc_21mer': 'CpG'}
+    host_lst = ['a_tract_21mer', 'g_tract_21mer', 'atat_21mer', 'gcgc_21mer']
+    strand_lst = ['STRAND1', 'STRAND2']
+    lgfz = 12
+    lbfz = 12
+    ttfz = 14
+
+    def __init__(self, bigtraj_folder, corr_folder):
+        self.bigtraj_folder = bigtraj_folder
+        self.corr_folder = corr_folder
+
+        self.d_corr_agent = None
+        self.ini_d_corr_agent()
+
+    def ini_d_corr_agent(self):
+        self.d_corr_agent = dict()
+        for host in self.host_lst:
+            self.d_corr_agent[host] = CorrelationScatterv0(host, self.bigtraj_folder, self.corr_folder)
+
+    def scatter_single(self, interaction_x, interaction_y, figsize, upper_or_lower_bb='upper', upper_or_lower_st='upper'):
+        """
+        interaction_x, interaction_y: HB1, HB2, HB3, BB1-BB5, ST1
+        """
+        fig, ax = plt.subplots(figsize=figsize)
+        for host in self.host_lst:
+            agent = self.d_corr_agent[host]
+            host_abbr = self.d_abbr_host[host]
+            for strand_id in self.strand_lst:
+                x_array = agent.get_data_array_by_interaction(interaction_x, strand_id, upper_or_lower_bb, upper_or_lower_st)
+                y_array = agent.get_data_array_by_interaction(interaction_y, strand_id, upper_or_lower_bb, upper_or_lower_st)
+                label = f'{host_abbr} {strand_id}'
+                ax.scatter(x_array, y_array, label=label)
+        ax.set_xlabel(f'{interaction_x} (kcal/mol/Å$^2$)', fontsize=self.lbfz)
+        ax.set_ylabel(f'{interaction_y} (kcal/mol/Å$^2$)', fontsize=self.lbfz)
+        ax.legend(fontsize=self.lgfz)
+        return fig, ax
